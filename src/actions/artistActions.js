f97cc68009ad09244e6572a6e6c19836b61c5bd7
Ben Beekman <bbeekman@gmail.com>
2020-02-18 11:23:00 -0800

    additional actions

--- src/actions/artistActions.js
+++ src/actions/artistActions.js
@@ -1,9 +1,33 @@
+import { getQuery } from '../services/rijksMuseumAPI';
 export const FETCH_ARTIST_LOADING = 'FETCH_ARTIST_LOADING';
 export const FETCH_ARTIST = 'FETCH_ARTIST';
+export const GET_ARTIST = 'GET_ARTIST';
+export const SET_ARTIST = 'SET_ARTIST';
 
 export const fetchArtist = artist => dispatch => {
   dispatch({ type: FETCH_ARTIST_LOADING });
-  return getArtist(artist)
+  return getQuery(artist)
+    .then(post => {
+      dispatch({
+        type: FETCH_ARTIST,
+        payload: post
+      });
+    });
+};
+
+export const getArtist = artist => dispatch => {
+  dispatch({ type: FETCH_ARTIST_LOADING });
+  return getQuery(artist)
+    .then(post => {
+      dispatch({
+        type: FETCH_ARTIST,
+        payload: post
+      });
+    });
+};
+export const setArtist = artist => dispatch => {
+  dispatch({ type: FETCH_ARTIST_LOADING });
+  return getQuery(artist)
     .then(post => {
       dispatch({
         type: FETCH_ARTIST,
